ee82d297ccd41f79774497342f104cd0a0065c8e
Luka <luka.luka.luka93@gmail.com>
2024-05-21 14:04:09 -0700

    connected App.js with imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import FiltersList from "./Filters/index";
-import filterData from "./data.js";
+import HotelsList from "./Hotels/index.js";
+import Filters from "./Filters/index";
+import {filterData, hotelData} from "./data.js";
 //TODO: import the data model for your list of filters
 
 const App = () => (
   <div className="App">
-    {/* TODO: pass your data model as a prop to your FiltersList component */}
-    <FiltersList filters={filterData} />
+    <Filters filters={filterData} />
+    <HotelsList hotels={hotelData}/>
   </div>
 );
 
