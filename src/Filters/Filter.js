929bb470223ae3ae2ea52bbd4a0d4191debdc28c
Luka <luka.luka.luka93@gmail.com>
2024-05-23 14:11:11 -0700

    updated filter.js in filter folder

--- src/Filters/Filter.js
+++ src/Filters/Filter.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { hotels } from "../data.js";
 
-/* functional component
-    - accepts the data model for a single filter as a prop
-    - uses that data to render the UI for a single filter in the list
-*/
-const Filter = (props) => (
-  <li className="filter">
-    <span>
-      <input type="checkbox" value={true} className="filter__checkbox" />
-      {props.filter.name}
-    </span>
-    <span className="filter__match-count">{props.filter.matchCount}</span>
-  </li>
-);
+const getMatchCount = (filterKey) =>
+  hotels.filter((hotel) => hotel[filterKey]).length;
+
+const Filter = (props) => {
+
+  const onChange = () => props.toggleFilter(props.filter.key);
+  const matchCount = getMatchCount(props.filter.key);
+  return (
+    <li className="filter">
+      <span>
+        <input
+          type="checkbox"
+          checked={props.isChecked}
+          onChange={onChange}
+          className="filter__checkbox"
+        />
+        {props.filter.display}
+      </span>
+      <span className="filter__match-count">{matchCount}</span>
+    </li>
+  );
+};
 
 export default Filter;
